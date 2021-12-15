8306fcbe9af3ef89a4c002b33a364f22b936b1d3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-12-15 16:37:05 +0100

    Refaktoring

--- Bupa.Bupa.Tests/Pages/HealthPage.cs
+++ Bupa.Bupa.Tests/Pages/HealthPage.cs
@@ -1,5 +1,4 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,47 +20,8 @@ namespace Bupa.Bupa.Tests.Pages
         public bool HasHeader => wait.Until(driver=> Header.Displayed);
     }
 
-    public class QuoteYourNamePage : BasePage
-    {
-        public QuoteYourNamePage(IWebDriver driver) : base(driver)
-        {
-        }
-
-        public IWebElement TitleCodeField => driver.FindElement(By.Id("Prospect_ApplicantDetails_TitleCode"));
-        public IWebElement FirstNameField => driver.FindElement(By.Id("Prospect_ApplicantDetails_FirstName"));
-        public IWebElement LastNameField => driver.FindElement(By.Id("Prospect_ApplicantDetails_LastName"));
-
-        public SelectElement TitleCodeComboBox => new SelectElement(TitleCodeField);
-
-
-        // Selenium.Support
-        public void Fill(string titleCode, string firstname, string lastname)
-        {
-            FirstNameField.SendKeys(firstname);
-            LastNameField.SendKeys(lastname);
-
-            TitleCodeComboBox.SelectByText(titleCode);
-            
-        }
-
-        public void Fill(TitleCodes titleCode, string firstname, string lastname)
-        {
-            FirstNameField.SendKeys(firstname);
-            LastNameField.SendKeys(lastname);
-
-            TitleCodeComboBox.SelectByValue(((int)titleCode).ToString());
-        }
-    }
-
    
-    public enum TitleCodes
-    {        
-        [Power(100, 3)]
-        Mr = 36,
-
-        [Power(200, 2)]
-        Miss = 35
-    }
+    
 
 
     
---
+++ Bupa.Bupa.Tests/Pages/QuoteYourNamePage.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Bupa.Bupa.Tests.Pages
+{
+    public class QuoteYourNamePage : BasePage
+    {
+        public QuoteYourNamePage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public IWebElement TitleCodeField => driver.FindElement(By.Id("Prospect_ApplicantDetails_TitleCode"));
+        public IWebElement FirstNameField => driver.FindElement(By.Id("Prospect_ApplicantDetails_FirstName"));
+        public IWebElement LastNameField => driver.FindElement(By.Id("Prospect_ApplicantDetails_LastName"));
+
+        public SelectElement TitleCodeComboBox => new SelectElement(TitleCodeField);
+
+
+        // Selenium.Support
+        public void Fill(string titleCode, string firstname, string lastname)
+        {
+            FirstNameField.SendKeys(firstname);
+            LastNameField.SendKeys(lastname);
+
+            TitleCodeComboBox.SelectByText(titleCode);
+            
+        }
+
+        public void Fill(TitleCodes titleCode, string firstname, string lastname)
+        {
+            FirstNameField.SendKeys(firstname);
+            LastNameField.SendKeys(lastname);
+
+            TitleCodeComboBox.SelectByValue(((int)titleCode).ToString());
+        }
+    }
+
+    public enum TitleCodes
+    {
+        [Power(100, 3)]
+        Mr = 36,
+
+        [Power(200, 2)]
+        Miss = 35
+    }
+
+
+}
