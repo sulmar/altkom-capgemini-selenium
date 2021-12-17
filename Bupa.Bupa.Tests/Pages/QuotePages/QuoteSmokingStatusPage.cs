e8e91c0375d0f7a05a3064781036b18ada021f21
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-12-17 09:39:24 +0100

    Fix

---
+++ Bupa.Bupa.Tests/Pages/QuotePages/QuoteSmokingStatusPage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bupa.Bupa.Tests.Pages
+{
+    public class QuoteSmokingStatusPage : QuotePage
+    {
+        public QuoteSmokingStatusPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        private IReadOnlyList<IWebElement> SmokeButtons => driver.FindElements(By.ClassName("you-smoke-btn"));
+
+        private IWebElement YesButton => SmokeButtons.First();
+        private IWebElement NoButton => SmokeButtons.Last();
+
+
+        public void Fill(bool smoke)
+        {
+            if (smoke)
+            {
+                YesButton.Click();
+            }
+            else
+            {
+                NoButton.Click();
+            }
+        }
+
+    }
+
+
+}
--- Bupa.Bupa.Tests/Pages/QuotePages/QuoteYourNamePage.cs
+++ Bupa.Bupa.Tests/Pages/QuotePages/QuoteYourNamePage.cs
@@ -1,35 +1,8 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Bupa.Bupa.Tests.Pages
 {
-    public class QuoteSmokingStatusPage : QuotePage
-    {
-        public QuoteSmokingStatusPage(IWebDriver driver) : base(driver)
-        {
-        }
-
-        private IReadOnlyList<IWebElement> SmokeButtons => driver.FindElements(By.ClassName("you-smoke-btn"));
-
-        private IWebElement YesButton => SmokeButtons.First();
-        private IWebElement NoButton => SmokeButtons.Last();
-
-
-        public void Fill(bool smoke)
-        {
-            if (smoke)
-            {
-                YesButton.Click();
-            }
-            else
-            {
-                NoButton.Click();
-            }
-        }
-
-    }
 
     public class QuoteYourNamePage : QuotePage
     {
